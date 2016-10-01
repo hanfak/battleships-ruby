3b2efff49050d9dbfaf5633c952a9ba81dfd0ae9
Hanif Fakira <fakira.work@gmail.com>
2016-10-01 11:35:48 +0100

    refactor: extract error to method dry
    - Issue with comparing for non square boards

--- lib/board.rb
+++ lib/board.rb
@@ -13,25 +13,29 @@ class Board
   private
   # given starting point places to the right
   def place_horizontally(ship, y_coord, x_coord)
-    raise "Out of bounds: starting position should be within game board boundary" if x_coord + ship.size > description[0].length
+    check_ship_fits_on_board(x_coord, ship, description)
 
     (1..ship.size).each do |amount_of_ship_parts|
       stored_x_coord = x_coord + amount_of_ship_parts - 1
 
-      @description[y_coord ][stored_x_coord] = ship
+      @description[y_coord][stored_x_coord] = ship
       ship.store_location(y_coord, stored_x_coord)
     end
   end
 
   # Given starting point places downwards
   def place_vertically(ship, y_coord, x_coord)
-    raise "Out of bounds: starting position should be within game board boundary" if y_coord + ship.size > description.length
+    check_ship_fits_on_board(y_coord, ship, description)
 
     (1..ship.size).each do |amount_of_ship_parts|
       stored_y_coord = y_coord + amount_of_ship_parts - 1
 
-      @description[stored_y_coord][x_coord  ] = ship
+      @description[stored_y_coord][x_coord] = ship
       ship.store_location(stored_y_coord, x_coord)
     end
+  end
+
+  def check_ship_fits_on_board(coord = nil, ship, description)
+    raise "Out of bounds: starting position should be within game board boundary" if coord + ship.size > description.length
   end
 end
