bcfda596407aff697580bc0133786e54f9af1bfd
Hanif Fakira <fakira.work@gmail.com>
2016-10-04 14:14:31 +0100

    refactor ship sunks to method

--- lib/board.rb
+++ lib/board.rb
@@ -15,15 +15,12 @@ class Board
   end
 
   def change_opponents_board_view(y_coord, x_coord)
-    space = description[y_coord][x_coord]
-    return :miss if space == SEA
-    space.change_status(y_coord, x_coord)
-    if ships.collect(&:position).collect(&:values).flatten.uniq == [:hit]
-      :won
-    else
-      space.hit_or_sunk
-    end
+    ship = description[y_coord][x_coord]
+    return :miss if ship == SEA
+    ship.change_status(y_coord, x_coord)
+    all_ships_sunk? ? :won : ship.hit_or_sunk
   end
+
 
 
   private
@@ -31,7 +28,7 @@ class Board
   # given starting point places to the right
   def place_horizontally(ship, y_coord, x_coord)
     check_ship_fits_on_board(x_coord, ship, description)
-    check_if_ship_already_exists_in_same_row(ship, y_coord, x_coord )
+    check_ship_already_exists_in_same_row(ship, y_coord, x_coord )
 
     (1..ship.size).each do |amount_of_ship_parts|
       stored_x_coord = x_coord + amount_of_ship_parts - 1
@@ -44,7 +41,7 @@ class Board
   # Given starting point places downwards
   def place_vertically(ship, y_coord, x_coord)
     check_ship_fits_on_board(y_coord, ship, description)
-    check_if_ship_already_exists_in_same_column(ship, y_coord, x_coord )
+    check_ship_already_exists_in_same_column(ship, y_coord, x_coord )
 
     (1..ship.size).each do |amount_of_ship_parts|
       stored_y_coord = y_coord + amount_of_ship_parts - 1
@@ -54,6 +51,11 @@ class Board
     end
   end
 
+  # checks that all parts of all the ships on the board, have been hit
+  def all_ships_sunk?
+    ships.collect(&:position).collect(&:values).flatten.uniq == [:hit]
+  end
+
   #### Errors ####
 
   # Either the ship is out of bounds and/or the ship size pushes ship out of bounds
@@ -61,14 +63,14 @@ class Board
     raise "Out of bounds: starting position should be within game board boundary" if coord + ship.size > description.length
   end
 
-  def check_if_ship_already_exists_in_same_row(ship, y_coord, x_coord )
+  def check_ship_already_exists_in_same_row(ship, y_coord, x_coord )
     # Goes through row, and finds if ship exists in the space where new ship will go
     unless (0..ship.size-1).select {|ele| description[y_coord] [x_coord + ele] != SEA }.empty?
       raise "Ship already there: Choose another position so not overlap with a ship in that place"
     end
   end
 
-  def check_if_ship_already_exists_in_same_column(ship, y_coord, x_coord )
+  def check_ship_already_exists_in_same_column(ship, y_coord, x_coord )
     # Goes through column, and finds if ship exists in the space where new ship will go
     unless (0..ship.size-1).select {|ele| description[y_coord + ele][x_coord] != SEA }.empty?
       raise "Ship already there: Choose another position so not overlap with a ship in that place"
